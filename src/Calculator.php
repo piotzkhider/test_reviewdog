9c1f1e94b65057cbade853f1a502171de513e3f9
piotzkhider <piotzkhider@gmail.com>
2019-01-15 01:19:25 +0900

    ひっかかっていたコードの修正

--- src/Calculator.php
+++ src/Calculator.php
@@ -2,22 +2,24 @@
 
 namespace Acme;
 
+use DateTimeImmutable;
+
 class Calculator
 {
     public $displayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyaaaayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy;
 
-    function div(int $a, int $b): int
+    public function div(int $a, int $b): int
     {
         return $a / $b;
     }
 
-    var $aaa;
+    public $aaa;
 
-    var $bbb;
+    public $bbb;
 
-    public function sayHello(DateTimeImutable $date): void
+    public function sayHello(DateTimeImmutable $date): void
     {
-        echo 'Hello, ' . $date->format('j. n. Y');
+        echo 'Hello, '.$date->format('j. n. Y');
     }
 
     public function aaa()
